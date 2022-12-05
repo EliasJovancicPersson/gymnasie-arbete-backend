e4d26e5bb3a40b619911c0f515f75c72f27c8e07
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-05 18:34:18 +0100

    schema done

--- backend/models/gradProjModel.js
+++ backend/models/gradProjModel.js
@@ -1,36 +1,38 @@
 const mongoose = require("mongoose");
 
 const subjects = [
-	"mattematik",
-	"samhällskunskap",
-	"historia",
-	"religion",
-	"teknik",
+  "naturvetenskapsprogrammet",
+  "teknikprogrammet",
+  "ekonomiprogrammet",
+  "humanistiskaprogrammet",
+  "samhällsvetenskapligaprogrammet",
+  "estetiskaprogrammet",
+  "internationall-baccalaureate",
 ];
 
 const gradProjModel = mongoose.Schema(
-	{
-		title: {
-			type: String,
-			requied: [true, "please add a title"],
-		},
-		author: {
-			type: String,
-			requied: [true, "please add a author"],
-		},
-		text: {
-			type: String,
-			requied: [true, "please add a text value"],
-		},
-		subject: {
-			type: String,
-			enum: subjects,
-			requied: [true, "please add a text value"],
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    title: {
+      type: String,
+      requied: [true, "please add a title"],
+    },
+    author: {
+      type: String,
+      requied: [true, "please add a author"],
+    },
+    text: {
+      type: String,
+      requied: [true, "please add a text value"],
+    },
+    subject: {
+      type: String,
+      enum: subjects,
+      requied: [true, "please add a text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("GradProject", gradProjModel);

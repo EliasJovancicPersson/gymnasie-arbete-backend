129e9f1844bcd30fa0ae836a7710af0dc92fb791
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-07 16:39:14 +0100

    fixed headers and response

--- backend/controllers/projController.js
+++ backend/controllers/projController.js
@@ -6,74 +6,83 @@ const Project = require("../models/gradProjModel");
 // @route   GET /api/goals
 // @access  Private
 const getGradProjs = asyncHandler(async (req, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "content-type": "application/json",
+  });
 
-	const projects = await Project.find();
+  const projects = await Project.find();
 
-	res.status(200).json({ projects });
+  res.status(200).json({ projects });
 });
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
 const setGradProj = asyncHandler(async (req, res) => {
-	if (
-		!req.body.title ||
-		!req.body.author ||
-		!req.body.text ||
-		!req.body.subject
-	) {
-		res.status(400);
-		throw new Error("please add all text fields");
-	}
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "content-type": "application/x-www-form-urlencoded",
+  });
 
-	const project = await Project.create({
-		titel: req.body.titel,
-		author: req.body.author,
-		text: req.body.text, //create project doc with text
-		subject: req.body.subject,
-	});
+  if (
+    !req.body.title ||
+    !req.body.author ||
+    !req.body.text ||
+    !req.body.subject
+  ) {
+    res.status(400);
+    throw new Error("please add all text fields" + "    " + req.body);
+  }
 
-	res.status(200).json({ project });
+  const project = await Project.create({
+    title: req.body.title,
+    author: req.body.author,
+    text: req.body.text, //create project doc with text
+    subject: req.body.subject,
+  });
+
+  res.status(200).json({ project } + req.body.title);
 });
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	if (!project) {
-		res.status(400);
-		throw new Error("goal not found");
-	}
+  if (!project) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
 
-	const updatedProject = await Project.findByIdAndUpdate(
-		req.params.id,
-		req.body,
-		{
-			new: true,
-		}
-	);
+  const updatedProject = await Project.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
 
-	res.status(200).json({ updatedProject });
+  res.status(200).json({ updatedProject });
 });
 
 // @desc    Delete goals
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	if (!project) {
-		res.status(400);
-		throw new Error("goal not found");
-	}
+  if (!project) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
 
-	await project.remove();
+  await project.remove();
 
-	res.status(200).json({ id: req.params.id });
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGradProjs, setGradProj, updateGradProj, deleteGradProj };
--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.all("/", function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "content-type",
+    "content-type": "application/json",
+  });
+  next();
 });
 
 app.use("/wiki", require("./routes/projRoutes"));
@@ -26,5 +26,5 @@ app.use("/wiki", require("./routes/projRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
-	console.log(`server started on port ${port}`);
+  console.log(`server started on port ${port}`);
 });

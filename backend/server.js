d1efe73c0cad17a67670824d281ad093478dbad3
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-12-07 15:45:12 +0100

    new headers

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ app.use(express.urlencoded({ extended: false }));
 
 app.all("/", function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
 });
 

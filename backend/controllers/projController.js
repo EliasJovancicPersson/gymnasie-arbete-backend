eb25a86b4aab7411e673daa11c9f0ee2f126b851
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-18 02:13:53 +0100

    fixed express

--- backend/controllers/projController.js
+++ backend/controllers/projController.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const asyncHandler = require("express-async-handler");
 const { v4: uuidv4 } = require("uuid");
 
@@ -7,103 +8,103 @@ const Project = require("../models/gradProjModel");
 // @route   GET /api/goals
 // @access  Private
 const getGradProjs = asyncHandler(async (req, res) => {
-	res.set({
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Headers": "*",
-		"content-type": "application/json",
-	});
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "content-type": "application/json",
+  });
 
-	const projects = await Project.find();
+  const projects = await Project.find();
 
-	res.status(200).json({ projects });
+  res.status(200).json({ projects });
 });
 
 // @desc    Set grad project without image
 // @route   POST /wiki
 const setGradProj = asyncHandler(async (req, res) => {
-	res.set({
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Headers": "*",
-		"content-type": "application/x-www-form-urlencoded",
-	});
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "content-type": "application/x-www-form-urlencoded",
+  });
 
-	if (
-		!req.body.title ||
-		!req.body.author ||
-		!req.body.text ||
-		!req.body.subject
-	) {
-		res.status(400);
-		throw new Error("please add all text fields" + "    " + req.body);
-	}
+  if (
+    !req.body.title ||
+    !req.body.author ||
+    !req.body.text ||
+    !req.body.subject
+  ) {
+    res.status(400);
+    throw new Error("please add all text fields" + "    " + req.body);
+  }
 
-	//if no file
+  //if no file
 
-	const id = uuidv4();
+  const id = uuidv4();
 
-	const project = await Project.create({
-		title: req.body.title,
-		author: req.body.author,
-		text: req.body.text, //create project doc with text
-		subject: req.body.subject,
-		_id: id,
-	});
+  const project = await Project.create({
+    title: req.body.title,
+    author: req.body.author,
+    text: req.body.text, //create project doc with text
+    subject: req.body.subject,
+    _id: id,
+  });
 
-	res.status(200).json(id);
+  res.status(200).json(id);
 });
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 const updateGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	if (!project) {
-		res.status(400);
-		throw new Error("goal not found");
-	}
+  if (!project) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
 
-	const updatedProject = await Project.findByIdAndUpdate(
-		req.params.id,
-		req.body,
-		{
-			new: true,
-		}
-	);
+  const updatedProject = await Project.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
 
-	res.status(200).json({ updatedProject });
+  res.status(200).json({ updatedProject });
 });
 
 // @desc    Delete project
 const deleteGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	if (!project) {
-		res.status(400);
-		throw new Error("goal not found");
-	}
+  if (!project) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
 
-	await project.remove();
+  await project.remove();
 
-	res.status(200).json({ id: req.params.id });
+  res.status(200).json({ id: req.params.id });
 });
 
 // @desc    Get seperate proj
 const getGradProj = asyncHandler(async (req, res) => {
-	res.set({
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Headers": "*",
-		"content-type": "application/json",
-	});
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "content-type": "application/json",
+  });
 
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	res.status(200).json({ project });
+  res.status(200).json({ project });
 });
 
 module.exports = {
-	getGradProjs,
-	setGradProj,
-	updateGradProj,
-	deleteGradProj,
-	getGradProj,
+  getGradProjs,
+  setGradProj,
+  updateGradProj,
+  deleteGradProj,
+  getGradProj,
 };

db322fe5a270a784bae29ce12506eb68742c237c
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-01-29 02:26:42 +0100

    loggedIn changed to authenticated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -99,7 +99,7 @@ exports.getUser = (req, res) => {
     }
     if (user) {
       return res.status(200).send({
-        loggedIn: true,
+        authenticated: true,
         id: toString(user.id),
         email: user.email,
         name: user.fullName,

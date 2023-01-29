d952a4307340ee8b57b2d3b102c27d4cd68bda3c
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-01-29 02:02:28 +0100

    potential fix for id issue

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -83,7 +83,7 @@ exports.signout = (req, res) => {
 };
 
 exports.getUser = (req, res) => {
-  User.findById(ObjectId(req.body.id)).exec((err, user) => {
+  User.findById(req.params.profileId).exec((err, user) => {
     if (err) {
       res.status(500).send({
         message: err,

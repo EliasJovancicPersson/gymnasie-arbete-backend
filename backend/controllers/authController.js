38404d13a7778a3d52ed2beb92cdd4ccb04ad3cb
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-01-29 01:54:41 +0100

    new get user path

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,82 +2,101 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 exports.signup = (req, res) => {
-	const user = new User({
-		fullName: req.body.fullName,
-		email: req.body.email,
-		role: req.body.role,
-		password: bcrypt.hashSync(req.body.password, 8),
-	});
+  const user = new User({
+    fullName: req.body.fullName,
+    email: req.body.email,
+    role: req.body.role,
+    password: bcrypt.hashSync(req.body.password, 8),
+  });
 
-	user.save((err, user) => {
-		if (err) {
-			res.status(500).send({
-				message: err,
-			});
-			return;
-		} else {
-			res.status(200).send({
-				message: "User Registered successfully",
-			});
-		}
-	});
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: err,
+      });
+      return;
+    } else {
+      res.status(200).send({
+        message: "User Registered successfully",
+      });
+    }
+  });
 };
 
 exports.signin = (req, res) => {
-	User.findOne({
-		email: req.body.email,
-	}).exec((err, user) => {
-		if (err) {
-			res.status(500).send({
-				message: err,
-			});
-			return;
-		}
-		if (!user) {
-			return res.status(404).send({
-				message: "User Not found.",
-			});
-		}
+  User.findOne({
+    email: req.body.email,
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: err,
+      });
+      return;
+    }
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found.",
+      });
+    }
 
-		//comparing passwords
-		var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-		if (!passwordIsValid) {
-			return res.status(401).send({
-				accessToken: null,
-				message: "Invalid Password!",
-			});
-		}
-		//signing token with user id
-		var token = jwt.sign(
-			{
-				id: user.id,
-			},
-			process.env.API_SECRET,
-			{
-				expiresIn: "10 days",
-			}
-		);
+    //comparing passwords
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!",
+      });
+    }
+    //signing token with user id
+    var token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.API_SECRET,
+      {
+        expiresIn: "10 days",
+      }
+    );
 
-		//responding to client request with user profile success message and access token as cookie
-		res.cookie("jwt", token, {
-			httpOnly: true,
-			secure: true,
-			sameSite: "None",
-			maxAge: 24 * 60 * 60 * 1000,
-		}); //send token as a cookie when logging in
-		res.status(200).send({
-			message: "Login successfull",
-		});
-	});
+    //responding to client request with user profile success message and access token as cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    }); //send token as a cookie when logging in
+    res.status(200).send({
+      message: "Login successfull",
+    });
+  });
 };
 
 exports.signout = (req, res) => {
-	//setting cookie to blank on signout
-	res.clearCookie("jwt", {
-		httpOnly: true,
-		secure: true,
-		sameSite: "None",
-		maxAge: 24 * 60 * 60 * 1000,
-	});
-	res.status(200).send({ message: "logged out" });
+  //setting cookie to blank on signout
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+  res.status(200).send({ message: "logged out" });
+};
+
+exports.getUser = (req, res) => {
+  User.findById(req.body.id).exec((err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: err,
+      });
+      return;
+    }
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found.",
+      });
+    }
+    if (user) {
+      return res.status(200).send(user);
+    }
+  });
 };

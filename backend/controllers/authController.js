6033c1222c2edbc6d3c90d49b9de48956e45db1b
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-01-30 00:17:09 +0100

    var change

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -103,7 +103,7 @@ exports.getUser = (req, res) => {
     if (user) {
       return res.status(200).send({
         authenticated: true,
-        user: {
+        currentUser: {
           id: user._id.toString(),
           email: user.email,
           name: user.fullName,

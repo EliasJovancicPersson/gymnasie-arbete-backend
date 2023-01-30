75ab74ed1563e59a7f44cde85a6cae464f3dbf4f
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-01-30 01:02:07 +0100

    new origin

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,6 +5,11 @@ const ErrorHandler = (err, req, res, next) => {
 
   console.log(statusCode);
 
+  res.set({
+    "Access-Control-Allow-Origin": "https://127.0.0.1:5500",
+    "Access-Control-Allow-Headers": "content-type",
+  });
+
   res.json({
     message: err.message,
     stack: process.env.Node_ENV === "production" ? null : err.stack,

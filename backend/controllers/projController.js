c54ebbfe8bfbdbbbe08e3caf501ce8d3317fd70e
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-04-13 00:10:45 +0200

    a

--- backend/controllers/projController.js
+++ backend/controllers/projController.js
@@ -4,7 +4,7 @@ const { findByIdAndUpdate, findOne } = require("../models/gradProjModel");
 const { BlobServiceClient } = require("@azure/storage-blob");
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(
-	process.env.AZURE_URI
+  process.env.AZURE_URI
 );
 const containerClient = blobServiceClient.getContainerClient("pdf");
 
@@ -14,105 +14,106 @@ const Project = require("../models/gradProjModel");
 // @route   GET /wiki/search
 // @access  Private
 const getGradProjsQuery = asyncHandler(async (req, res) => {
-	const filter = Object.keys(req.query);
+  const filter = Object.keys(req.query);
 
-	const regex = new RegExp(req.query[filter[0]], "i");
+  const regex = new RegExp(req.query[filter[0]], "i");
 
-	const projects = await Project.find({ [filter[0]]: regex });
+  const projects = await Project.find({ [filter[0]]: regex });
 
-	res.status(200).json({ projects });
+  res.status(200).json({ projects });
 });
 
 // @desc    Get all projects
 // @route   GET /wiki
 const getGradProjs = asyncHandler(async (req, res) => {
-	const projects = await Project.find();
+  const projects = await Project.find();
 
-	res.status(200).json({ projects });
+  res.status(200).json({ projects });
 });
 
-// @desc    Set grad project without image
+// @desc    Set grad project
 // @route   POST /wiki
 const setGradProj = asyncHandler(async (req, res) => {
-	if (!req.body) {
-		res.status(400);
-		throw new Error("please add all text fields" + "    " + req.body["title"]);
-	}
-	if (req.files) {
-		//upload pdf
-		let { buffer } = req.files["pdf"][0]; //TODO : add support for multiple files, should be just doing a foreach
+  if (!req.body || !req.files) {
+    res.status(400);
+    throw new Error("please add all text and file fields");
+  }
+  console.log(req.body)
+  if (req.files) {
+    //upload pdf
+    let { buffer } = req.files["pdf"][0]; //TODO : add support for multiple files, should be just doing a foreach
 
-		const fileName = Date.now() + "-" + uuidv4();
-		console.log(fileName);
+    const fileName = Date.now() + "-" + uuidv4();
+    console.log(fileName);
 
-		const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-		const response = await blockBlobClient.uploadData(buffer, {
-			blobHTTPHeaders: {
-				blobContentType: "application/pdf",
-			},
-		});
-		var pdfUrl = process.env.AZURE_URL + fileName;
-		//create url for pdf
-	}
-	const id = uuidv4();
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+    const response = await blockBlobClient.uploadData(buffer, {
+      blobHTTPHeaders: {
+        blobContentType: "application/pdf",
+      },
+    });
+    var pdfUrl = process.env.AZURE_URL + fileName;
+    //create url for pdf
+  }
+  const id = uuidv4();
 
-	const project = await Project.create({
-		title: req.body.title,
-		author: req.body.author,
-		pdf: pdfUrl, //create project doc with text
-		subject: req.body.subject,
-		_id: id,
-	});
-	res.status(200).json({ id: id });
+  const project = await Project.create({
+    title: req.body.title,
+    author: req.body.author,
+    pdf: pdfUrl, //create project doc with text
+    subject: req.body.subject,
+    _id: id,
+  });
+  res.status(200).json({ id: id });
 });
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 const updateGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	if (!project) {
-		res.status(400);
-		throw new Error("goal not found");
-	}
+  if (!project) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
 
-	const updatedProject = await Project.findByIdAndUpdate(
-		req.params.id,
-		req.body,
-		{
-			new: true,
-		}
-	);
+  const updatedProject = await Project.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
 
-	res.status(200).json({ updatedProject });
+  res.status(200).json({ updatedProject });
 });
 
 // @desc    Delete project
 const deleteGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	if (!project) {
-		res.status(400);
-		throw new Error("goal not found");
-	}
+  if (!project) {
+    res.status(400);
+    throw new Error("goal not found");
+  }
 
-	await project.remove();
+  await project.remove();
 
-	res.status(200).json({ id: req.params.id });
+  res.status(200).json({ id: req.params.id });
 });
 
 // @desc    Get seperate proj
 const getGradProj = asyncHandler(async (req, res) => {
-	const project = await Project.findById(req.params.id);
+  const project = await Project.findById(req.params.id);
 
-	res.status(200).json({ project });
+  res.status(200).json({ project });
 });
 
 module.exports = {
-	getGradProjs,
-	setGradProj,
-	updateGradProj,
-	deleteGradProj,
-	getGradProj,
-	getGradProjsQuery,
+  getGradProjs,
+  setGradProj,
+  updateGradProj,
+  deleteGradProj,
+  getGradProj,
+  getGradProjsQuery,
 };
